292d70fbda381e8679626a9e2652dadf8929305a
Luis Leiva <luis.leiva@utp.ac.pa>
2025-04-20 17:59:15 -0500

    feat: agregar funcionalidad para subir imágenes al crear blog

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
+import uuid
 
 
 # MODELOS
 
 class Blog(models.Model):
+    def blog_image_path(instance, filename):
+        ext = filename.split('.')[-1]
+        return f'blogs/imageblog_{uuid.uuid4()}.{ext}'
+
     title = models.CharField(max_length=200)
     content = models.TextField()
+    image = models.ImageField(upload_to=blog_image_path, blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
