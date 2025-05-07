fcb6208a2b9b8908fe01e7502b55f71b2e60250a
Luis Leiva <luis.leiva@utp.ac.pa>
2025-05-07 00:57:20 -0500

    feat: add tags functionality to Blog model and update related templates and views

--- blogapp/models.py
+++ blogapp/models.py
@@ -43,6 +43,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     image = models.ImageField(upload_to=blog_image_path, blank=True, null=True)
+    tags = models.JSONField(default=list, blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
